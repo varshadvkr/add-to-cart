4876456ada7e6bea6ad8bbd59b239c2bd1dd046f
varshadvkr <152770617+varshadvkr@users.noreply.github.com>
2023-12-12 00:26:45 +0530

    Add files via upload

---
+++ product.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/service/api.service';
+import { CartService } from 'src/app/service/cart.service';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit{
+  counter : number = 1;
+  public productList : any ;
+  public filterCategory : any
+  searchKey:string ="";
+  constructor(private api : ApiService, private cartService : CartService) { }
+
+  ngOnInit(): void {
+    this.api.getProduct()
+    .subscribe(res=>{
+      this.productList = res;
+      this.filterCategory = res;
+      this.productList.forEach((a:any) => {
+        if(a.category ==="women's clothing" || a.category ==="men's clothing"){
+          a.category ="fashion"
+        }
+        Object.assign(a,{quantity:1,total:a.price});
+      });
+      console.log(this.productList)
+    });
+
+    this.cartService.search.subscribe((val:any)=>{
+      this.searchKey = val;
+    })
+  }
+  
+  plus(){
+    this.counter +=1;
+  }
+  minus(){
+      if(this.counter >1){
+        this.counter -=1;
+      }
+    }
+  addtocart(item: any){
+    this.cartService.addtoCart(item);
+    alert("product added successfully in your cart")
+  }
+  filter(category:string){
+    this.filterCategory = this.productList
+    .filter((a:any)=>{
+      if(a.category == category || category==''){
+        return a;
+      }
+    })
+  }
+}
